10a2f372124490346f81d679d319354388edb175
Ashish Dhamala <ashish.dhamala2015@gmail.com>
2020-10-01 22:11:50 +0100

    todo api for list, store, delete, mark as done and undone

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('todos', [TodoController::class, 'index']);
+Route::post('store-todo', [TodoController::class, 'store']);
+Route::post('delete-todo', [TodoController::class, 'delete']);
+Route::post('todo/mark-as-done', [TodoController::class, 'markAsDone']);
+Route::post('todo/mark-as-undone', [TodoController::class, 'markAsUnDone']);
